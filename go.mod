7207f98e0726ce49679649d9d708e09d7a267c72
Apurba Sundar Nayak <apurbasundar2002@gmail.com>
2025-01-11 18:58:31 +0530

    modified something

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/alucard777/go-todo-backend
+
+go 1.23.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	go.mongodb.org/mongo-driver v1.17.2
+)
--- routes/routes.go
+++ routes/routes.go
@@ -1,5 +1,19 @@
 package routes
 
 import (
-	"go-todo-backend/middleware"
+	"github.com/alucard777/go-todo-backend/controller"
+	"github.com/gorilla/mux"
 )
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/tasks", controller.GetAllTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/task", controller.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/tasks/{id}", controller.UpdateTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/undoTask/{id}", controller.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteTask/{id}", controller.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteTasks/", controller.DeleteTasks).Methods("DELETE", "OPTIONS")
+
+	return router
+}
